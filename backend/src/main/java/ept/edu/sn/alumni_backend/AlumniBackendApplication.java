cc5300b8069d92138c278ef92f9ec921943a4453
sowibrr <ibous2005@gmail.com>
2026-07-02 10:57:10 +0000

    Initialisation : Spring Boot et Angular

---
+++ backend/src/main/java/ept/edu/sn/alumni_backend/AlumniBackendApplication.java
@@ -0,0 +1,13 @@
+package ept.edu.sn.alumni_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumniBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ept/edu/sn/alumni_backend/AlumniBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ept.edu.sn.alumni_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumniBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
