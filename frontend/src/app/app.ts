1283eb0e71f6fa26a32c692a8ac051ac5a3631c8
sowibrr <ibous2005@gmail.com>
2026-07-04 22:54:42 +0000

    ajout de docker

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ButtonDirective } from 'primeng/button';
+// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ButtonDirective],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  protected readonly title = signal('Alumni EPT');
 }
