72a7f8049d797e76bd1109632447e0dc4228b69f
sowibrr <ibous2005@gmail.com>
2026-07-10 23:19:37 +0000

    config de docker watch pour le mode developpement

---
+++ backend/src/main/java/ept/edu/sn/alumni_backend/TestController.java
@@ -0,0 +1,15 @@
+package ept.edu.sn.alumni_backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping
+    public String direBonjour() {
+        return "Le Backend Spring Boot est bien connecté :)";
+    }
+}
