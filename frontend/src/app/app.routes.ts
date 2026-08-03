71c114cf31862c5cf7168a376247c4aa9e687b4a
sowibrr <ibous2005@gmail.com>
2026-08-03 22:33:14 +0000

    feat(auth): mise en place des routes et leur gestion(securité)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'connexion', pathMatch: 'full' },
+  {
+    path: 'connexion',
+    loadComponent: () =>
+      import('./features/auth/connexion/connexion').then((m) => m.Connexion)
+  },
+  {
+    path: 'accueil',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/accueil/accueil').then((m) => m.Accueil)
+  }
+];
