3b931ec0344130ce8809492266d39e2589d0e2cf
sowibrr <ibous2005@gmail.com>
2026-08-03 22:31:54 +0000

    feat(auth): ajout du component d'authentification

---
+++ frontend/src/app/features/auth/connexion/connexion.ts
@@ -0,0 +1,60 @@
+import { Component, inject, signal } from '@angular/core';
+import { AuthService } from '../../../core/services/auth/auth-service';
+import { Router } from '@angular/router';
+import { email, form, required, FormField } from '@angular/forms/signals';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { MessageModule } from 'primeng/message';
+
+@Component({
+  selector: 'app-connexion',
+  imports: [FormField, InputTextModule, PasswordModule, ButtonModule, MessageModule],
+  templateUrl: './connexion.html',
+  styleUrl: './connexion.scss',
+})
+export class Connexion {
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+
+  protected readonly modele = signal({
+    email: '',
+    password: ''
+  });
+
+  protected readonly formulaire = form(this.modele,
+    (champ) => {
+      required(champ.email, {message: "L'email est obligatoire"});
+      email(champ.email, {message: "Le format d'email n'est pas valide"});
+      required(champ.password, {message: "Le mot de passe est obligatoire"});
+    }
+  )
+
+  protected readonly enChargement = signal(false);
+  protected readonly messageErreur = signal<string | null>(null);
+  protected readonly afficherMdp = signal(false);
+
+  seConnecter() {
+    if (this.formulaire().invalid()) {
+      return;
+    }
+
+    this.enChargement.set(true);
+    this.messageErreur.set(null);
+
+    const { email, password } = this.modele();
+
+    this.authService.login(email, password).subscribe({
+      next: () => {
+        this.enChargement.set(false);
+        this.router.navigate(['/accueil']);
+      },
+      error: (err) => {
+        this.enChargement.set(false);
+        const msg = err?.error?.message || 'Une erreur est survenue lors de la connexion';
+        this.messageErreur.set(msg);
+      }
+    })
+  }
+}
