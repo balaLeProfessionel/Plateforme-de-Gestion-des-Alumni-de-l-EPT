6b93da034632cd0d04e1d7d51186daece6ca0406
sowibrr <ibous2005@gmail.com>
2026-08-03 22:30:05 +0000

    fix(auth): ajout de router outlet

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,17 +2,22 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
+import { EptPreset } from './theme/ept-preset';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     providePrimeNG({
             theme: {
-                preset: Aura
+                preset: EptPreset,
+                options: {
+                  darkModeSelector: false
+                }
             }
         })
   ]
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { ButtonDirective } from 'primeng/button';
+import { RouterOutlet } from "@angular/router";
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ButtonDirective],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
