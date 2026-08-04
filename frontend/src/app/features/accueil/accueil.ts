93856bdbdf8d9831edefef783040cd2a94d880e7
sowibrr <ibous2005@gmail.com>
2026-08-04 19:21:52 +0000

    refactor(auth): ajout d'un bouton Logout pour tester la deconnexion

--- frontend/src/app/features/accueil/accueil.ts
+++ frontend/src/app/features/accueil/accueil.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../core/services/auth/auth-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-accueil',
@@ -9,4 +10,10 @@ import { AuthService } from '../../core/services/auth/auth-service';
 })
 export class Accueil {
   protected readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+
+  seDeconnecter(): void {
+    this.authService.logout();
+    this.router.navigate(['/connexion']);
+  }
 }
