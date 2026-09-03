b79097a7731aff94e1b276914744032ce89adfcb
sowibrr <ibous2005@gmail.com>
2026-09-03 09:25:01 +0000

    feat(parcours): CRUD des experiences professionnelles avec DTO et mapper

--- backend/src/main/java/ept/edu/sn/alumni_backend/parcours/controller/ExperienceProfessionelleController.java
+++
@@ -1,8 +0,0 @@
-package ept.edu.sn.alumni_backend.parcours.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class ExperienceProfessionelleController {
-
-}
--- backend/src/main/java/ept/edu/sn/alumni_backend/parcours/service/ExperienceProfessionelleService.java
+++
@@ -1,8 +0,0 @@
-package ept.edu.sn.alumni_backend.parcours.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ExperienceProfessionelleService {
-
-}
